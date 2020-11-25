680097d699da6edb53f9116930bf95beff3b9835
iervan <iervanfirdiansyah1998@gmail.com>
2020-11-26 04:22:30 +0700

    update

--- resources/views/admin/pages/order/south.blade.php
+++ resources/views/admin/pages/order/south.blade.php
@@ -4,7 +4,7 @@
 <section class="content-header">
     <h1>
       Order Region South
-      <small>preview of simple tables</small>
+      <small>Eli Zita @ Universitas Amikom Yogyakarta</small>
     </h1>
     <ol class="breadcrumb">
       <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
